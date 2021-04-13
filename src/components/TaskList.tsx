2bd90f9fd62ad6391ffc786c18563f29424b88f0
Gabriel Vicente <vicente.gabrielv@gmail.com>
2021-04-12 23:12:44 -0300

    Desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,38 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === '') return;
+
+    const newTask:Task = {
+      id: Math.random(),
+      title : newTaskTitle,
+      isComplete: false,
+    }
+
+    const newArrTasks = [...tasks];
+    newArrTasks.push(newTask);
+
+    setTasks(newArrTasks);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const findTaks = tasks.find(task => task.id === id);
+    const newArrTasks = tasks.filter(task => task.id !== id);
+
+
+    if(findTaks){
+      findTaks.isComplete = !findTaks.isComplete;
+      newArrTasks.push(findTaks);
+    }
+
+    setTasks(newArrTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newArrTasks = tasks.filter(task => task.id !== id);
+    setTasks(newArrTasks);
   }
 
   return (
@@ -38,7 +61,7 @@ export function TaskList() {
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
+          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask} >
             <FiCheckSquare size={16} color="#fff"/>
           </button>
         </div>
